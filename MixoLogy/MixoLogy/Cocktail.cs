f93dcce9d568279a93df683c96f731489c3eb25c
Veronika Kolesnikova <VKolesnikova@rightpoint.com>
2019-04-10 17:45:59 -0400

    Add project files.

---
+++ MixoLogy/MixoLogy/Cocktail.cs
@@ -0,0 +1,46 @@
+﻿namespace MixoLogy
+{
+    internal class Cocktail
+    {
+        public string StrDrink { get; set; }
+        public string DateModified { get; set; }
+        public string IdDrink { get; set; }
+        public string StrAlcoholic { get; set; }
+        public string StrCategory { get; set; }
+        public string StrDrinkThumb { get; set; }
+        public string StrGlass { get; set; }
+        public string StrIBA { get; set; }
+        public string StrIngredient1 { get; set; }
+        public string StrIngredient10 { get; set; }
+        public string StrIngredient11 { get; set; }
+        public string StrIngredient12 { get; set; }
+        public string StrIngredient13 { get; set; }
+        public string StrIngredient14 { get; set; }
+        public string StrIngredient15 { get; set; }
+        public string StrIngredient2 { get; set; }
+        public string StrIngredient3 { get; set; }
+        public string StrIngredient4 { get; set; }
+        public string StrIngredient5 { get; set; }
+        public string StrIngredient6 { get; set; }
+        public string StrIngredient7 { get; set; }
+        public string StrIngredient8 { get; set; }
+        public string StrIngredient9 { get; set; }
+        public string StrInstructions { get; set; }
+        public string StrMeasure1 { get; set; }
+        public string StrMeasure10 { get; set; }
+        public string StrMeasure11 { get; set; }
+        public string StrMeasure12 { get; set; }
+        public string StrMeasure13 { get; set; }
+        public string StrMeasure14 { get; set; }
+        public string StrMeasure15 { get; set; }
+        public string StrMeasure2 { get; set; }
+        public string StrMeasure3 { get; set; }
+        public string StrMeasure4 { get; set; }
+        public string StrMeasure5 { get; set; }
+        public string StrMeasure6 { get; set; }
+        public string StrMeasure7 { get; set; }
+        public string StrMeasure8 { get; set; }
+        public string StrMeasure9 { get; set; }
+        public string StrVideo { get; set; }
+    }
+}
---
+++ MixoLogy/MixoLogy/MainPage.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using CsvHelper;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace MixoLogy
+{
+    public partial class MainPage : ContentPage
+    {
+        private int _pg = 1;
+        private List<Cocktail> _cocktails;
+        public MainPage()
+        {
+            InitializeComponent();
+            _cocktails = LoadCocktailsCollection();
+            CreateGrid(_cocktails);
+            
+        }
+
+        public interface IFileService
+        {
+            StreamReader GetFileStream(string name);
+        }
+
+        private List<Cocktail> LoadCocktailsCollection()
+        {
+            int imageDimension = Device.RuntimePlatform == Device.iOS ||
+                                 Device.RuntimePlatform == Device.Android ? 240 : 120;
+
+            var assembly = typeof(Cocktail).GetTypeInfo().Assembly;
+            Stream stream = assembly.GetManifestResourceStream("MixoLogy.Resources.all_drinks.csv");
+            //Stream stream = assembly.GetManifestResourceStream(fileName);
+            //var csvData = DataTable.New.ReadLazy(stream);
+            //return csvData.RowsAs<Cocktail>().ToList();
+            var list = new List<Cocktail>();
+            using (stream)
+            {
+                if (stream != null)
+                    using (var reader = new StreamReader(stream))
+                    {
+                        using (var csv = new CsvReader(reader))
+                        {
+
+                            csv.Configuration.Delimiter = ",";
+                            csv.Configuration.PrepareHeaderForMatch = (header, index) => header.ToLower();
+                            while (csv.Read())
+                            {
+                                var row = csv.GetRecord<Cocktail>();
+                                if (row == null)
+                                {
+                                    break;
+                                }
+                                list.Add(row);
+                            }
+                        }
+                    }
+            }
+
+            return list;
+        }
+
+        private void CreateGrid(List<Cocktail> cocktails)
+        {
+            if (cocktails != null && cocktails.Any())
+            {
+                if (flexLayout.Children.Count > 1)
+                {
+                    flexLayout.Children.RemoveAt(flexLayout.Children.Count - 1);
+                }
+
+                // Create an Image object for each bitmap
+                foreach (var cocktail in cocktails.Skip((_pg - 1) * 10).Take(_pg * 10))
+                {
+                    StackLayout stackLayout = new StackLayout();
+                    var image = new ImageButton()
+                    {
+                        Source = ImageSource.FromUri(new Uri(cocktail.StrDrinkThumb)),
+                        WidthRequest = 140,
+                        BorderColor = Color.Aquamarine,
+                        BorderWidth = 1
+                    };
+                    stackLayout.Children.Add(image);
+                    image.Margin = new Thickness(15, 15, 15, 15);
+                    image.VerticalOptions = LayoutOptions.CenterAndExpand;
+                    image.HorizontalOptions = LayoutOptions.CenterAndExpand;
+                    flexLayout.Children.Add(stackLayout);
+                }
+                var button = new Button
+                {
+                    Text = "Load More",
+                    Visual = new VisualMarker.MaterialVisual(),
+                    BackgroundColor = Color.HotPink,
+                    HorizontalOptions = LayoutOptions.CenterAndExpand,
+                    WidthRequest = 330,
+                    Margin = new Thickness(5, 5, 5, 5)
+                };
+                button.Clicked += LoadMore;
+                flexLayout.Children.Add(button);
+
+                _pg++;
+            }
+        }
+
+        private void LoadMore(object sender, EventArgs e)
+        {
+            CreateGrid(_cocktails);
+        }
+    }
+}
