7f076af26f718f9020a381e7f233dad0732bde78
Veronika Kolesnikova <VKolesnikova@rightpoint.com>
2019-04-11 17:49:12 -0400

    Add icons, sticky bottom navigation, fix layout

--- MixoLogy/MixoLogy/MainPage.xaml.cs
+++ MixoLogy/MixoLogy/MainPage.xaml.cs
@@ -4,8 +4,8 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using CsvHelper;
-using Xamarin.Essentials;
 using Xamarin.Forms;
+using Xamarin.Forms.StyleSheets;
 
 namespace MixoLogy
 {
@@ -16,9 +16,21 @@ namespace MixoLogy
         public MainPage()
         {
             InitializeComponent();
+            Resources.Add(StyleSheet.FromAssemblyResource(
+                typeof(MainPage).GetTypeInfo().Assembly,
+                "MixoLogy.Assets.styles.css"));
+
             _cocktails = LoadCocktailsCollection();
             CreateGrid(_cocktails);
-            
+            var button = new Button
+            {
+                Text = "Load More",
+                BackgroundColor = Color.HotPink,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                Margin = new Thickness(5, 5, 5, 40)
+            };
+            button.Clicked += LoadMore;
+            stackLayout.Children.Add(button);
         }
 
         public interface IFileService
@@ -67,47 +79,62 @@ namespace MixoLogy
         {
             if (cocktails != null && cocktails.Any())
             {
-                if (flexLayout.Children.Count > 1)
-                {
-                    flexLayout.Children.RemoveAt(flexLayout.Children.Count - 1);
-                }
-
-                // Create an Image object for each bitmap
                 foreach (var cocktail in cocktails.Skip((_pg - 1) * 10).Take(_pg * 10))
                 {
                     StackLayout stackLayout = new StackLayout();
-                    var image = new ImageButton()
+                    var image = new ImageButton
                     {
                         Source = ImageSource.FromUri(new Uri(cocktail.StrDrinkThumb)),
                         WidthRequest = 140,
                         BorderColor = Color.Aquamarine,
-                        BorderWidth = 1
+                        BorderWidth = 1,
+                        CommandParameter = cocktail
                     };
-                    stackLayout.Children.Add(image);
                     image.Margin = new Thickness(15, 15, 15, 15);
                     image.VerticalOptions = LayoutOptions.CenterAndExpand;
                     image.HorizontalOptions = LayoutOptions.CenterAndExpand;
+                    image.Clicked += ImageOnClicked;
+                    stackLayout.Children.Add(image);
                     flexLayout.Children.Add(stackLayout);
                 }
-                var button = new Button
-                {
-                    Text = "Load More",
-                    Visual = new VisualMarker.MaterialVisual(),
-                    BackgroundColor = Color.HotPink,
-                    HorizontalOptions = LayoutOptions.CenterAndExpand,
-                    WidthRequest = 330,
-                    Margin = new Thickness(5, 5, 5, 5)
-                };
-                button.Clicked += LoadMore;
-                flexLayout.Children.Add(button);
+                
 
                 _pg++;
+            }
+        }
+
+        private async void ImageOnClicked(object sender, EventArgs e)
+        {
+            var btn = sender as ImageButton;
+            var cocktail = btn?.CommandParameter as Cocktail;
+            FieldInfo[] fields = typeof(Cocktail).GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
+            var ingredients = fields.Where(x => x.Name.Contains("StrIngredient") && x.GetValue(cocktail).ToString() != "").ToList();
+            var measurements = fields.Where(x => x.Name.Contains("StrMeasure") && x.GetValue(cocktail).ToString() != "").ToList();
+
+            var instructions = "Ingredients:\n";
+            for (var i = 0; i < ingredients.Count; i++)
+            {
+                instructions += ingredients[i].GetValue(cocktail) + " " + measurements[i].GetValue(cocktail) + "\n";
+            }
+
+            if (cocktail != null)
+            {
+                var answer = await DisplayAlert(cocktail.StrDrink, instructions, "Continue", "Close");
+                if (answer)
+                {
+                    await DisplayAlert(cocktail.StrDrink, "Recipe: " + cocktail.StrInstructions, "Close");
+                }
             }
         }
 
         private void LoadMore(object sender, EventArgs e)
         {
             CreateGrid(_cocktails);
+        }
+
+        private void RandomRecipe(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
