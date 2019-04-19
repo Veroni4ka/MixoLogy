8a1b3e643afaf1d2689419da25fcde4d6c0e2301
Veronika Kolesnikova <VKolesnikova@rightpoint.com>
2019-04-19 16:44:50 -0400

    Add barcode scan screen

--- MixoLogy/MixoLogy/BarcodeScan.xaml.cs
+++ MixoLogy/MixoLogy/BarcodeScan.xaml.cs
@@ -3,8 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Plugin.Media;
+using Xamarin.Essentials;
+using Plugin.Media.Abstractions;
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
 using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
 using Xamarin.Forms.Xaml;
 
 namespace MixoLogy
@@ -32,6 +37,30 @@ namespace MixoLogy
 	    private void RandomRecipeBtn_OnClicked(object sender, EventArgs e)
 	    {
 	        Navigation.PushAsync(new RandomRecipe());
+        }
+
+	    private async void ScanBtn_OnClicked(object sender, EventArgs e)
+	    {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("No Camera", ":( No camera available.", "OK");
+                return;
+            }
+
+            var file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions());
+
+            if (file == null)
+                return;
+
+            await DisplayAlert("File Location", file.Path, "OK");
+
+            photoImage.Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                return stream;
+            });
         }
 	}
 }
--- MixoLogy/MixoLogy/RandomRecipe.xaml.cs
+++ MixoLogy/MixoLogy/RandomRecipe.xaml.cs
@@ -20,6 +20,7 @@ namespace MixoLogy
 		{
 			InitializeComponent ();
             DisplayRecipe();
+            ToggleAccelerometer();
 		    Accelerometer.ShakeDetected += Accelerometer_ShakeDetected;
         }
 
